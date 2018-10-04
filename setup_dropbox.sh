8bf10325a1ea2bbcb737e59acb8180a269d66786
azhang / meepslive <andrewzhang100@gmail.com>
2018-10-04 12:53:42 -0400

    Create setup_dropbox.sh

---
+++ setup_dropbox.sh
@@ -0,0 +1,11 @@
+cp -a ~/Desktop ~/Dropbox/Desktop
+rm -rf ~/Desktop
+ln -s ~/Dropbox/Desktop ~/Desktop
+
+cp -a ~/Downloads ~/Dropbox/ubuntu/Downloads
+rm -rf ~/Downloads
+ln -s ~/Dropbox/ubuntu/Downloads ~/Downloads
+
+cp -a ~/Pictures ~/Dropbox/ubuntu/Pictures
+rm -rf ~/Pictures
+ln -s ~/Dropbox/ubuntu/Pictures ~/Pictures
