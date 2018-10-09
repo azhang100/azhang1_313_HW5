99e111014500044a78f35b5bde3cbb1fbb698ea6
azhang100 <andrewzhang100@gmail.com>
2018-10-09 14:34:16 -0400

    crediential

--- git_repos.sh
+++ git_repos.sh
@@ -1,12 +1,17 @@
 cd ~
 
+git config credential.helper store
 # git clone "https://github.com/azhang100/ubuntu_setup.git"
+git config credential.helper store
 git clone "https://github.com/PipeDreamRobotics/radPiper.git"
+git config credential.helper store
 git clone "https://github.com/joshs333/PipeTodd.git"
+git config credential.helper store
 git clone "https://gitlab.com/azhang100/16720.git"
 
 # bitbucket ssh keys
 mv ~/ubuntu_setup/.ssh .ssh
 eval `ssh-agent`
 
+git config credential.helper store
 git clone "https://bitbucket.org/cmusubt/rgbd-safeguard"
